9ce31d548c4a8f10146f8b3e10f2e237fc283efd
bulatislamov <bulatletter@gmail.com>
2022-10-23 18:09:09 +0200

    Initial Commit

---
+++ SDL_WindowTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SDL_WindowTest
+//
+//  Created by Bulat Islamov on 23.10.22..
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
