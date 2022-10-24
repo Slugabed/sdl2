fe514dccbfb92c153234a751f0a35d6bb3117a53
Fyodor Shchukin <ted888@ya.ru>
2022-10-25 00:10:21 +0300

    Texture manager errors and fix windows crash

--- SDL_WindowTest/TextureManager.cpp
+++ SDL_WindowTest/TextureManager.cpp
@@ -9,8 +9,21 @@
 
 SDL_Texture* TextManager::LoadTexture(const char *fileName, SDL_Renderer *renderer) {
     SDL_Surface *tmpSurface = IMG_Load(fileName);
+    if (tmpSurface == NULL)
+    {
+        std::cout << "Can't load image " << fileName << "!" << std::endl;
+        return NULL;
+    }
+
     SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, tmpSurface);
-    SDL_FreeSurface(tmpSurface);
+    if (texture == NULL)
+    {
+        std::cout << "Can't create texture for " << fileName << "!" << std::endl;
+        return NULL;
+    }
+
+    // This line break Windows build
+    //SDL_FreeSurface(tmpSurface);
     
     return texture;
 }
